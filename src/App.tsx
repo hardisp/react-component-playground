b43cd3fec600385f9466b9fc2d531ee31748abe6
Hardi <jiksdi12@gmail.com>
2026-02-06 19:31:30 +0800

    feat: initial setup for demo playground

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { Select } from "./components";
+import type { SelectOptionProps } from "@hrdi/searchable-select";
+
+const options: SelectOptionProps[] = [
+  { value: "1", label: "User Icon" },
+  { value: "2", label: "Settings Icon" },
+  { value: "3", label: "Notification Bell" },
+  { value: "4", label: "Search Icon" },
+  { value: "5", label: "Logout Icon" },
+];
+export default function App() {
+  const [configure, setConfigure] = useState({
+    isMultiple: true,
+    isSearchable: true,
+    outlined: true
+  });
+     const [value, setValue] = useState<SelectOptionProps[] | SelectOptionProps>([]); 
+  return (
+    <div className="p-10">
+      <div className="min-h-screen relative">
+        <h2 className="mb-4 text-xl font-semibold">
+          Hardi Searchable Select Example
+        </h2>
+
+        <Select value={value} onChange={setValue} multiple={configure.isMultiple} searchable={configure.isSearchable} outline={configure.outlined} options={options} />
+
+        <div className="flex gap-4 mt-6">
+          <button
+            className="rounded-full"
+            onClick={() => {
+              setConfigure((_prev) => {
+                return {
+                  ..._prev,
+                  isMultiple: !_prev.isMultiple,
+                };
+              });
+              setValue([]);
+            }}
+          >
+            Multiple: {configure.isMultiple ? "ON" : "OFF"}
+          </button>
+          <button
+            className="rounded-full"
+            onClick={() => {
+              setConfigure((_prev) => {
+                return {
+                  ..._prev,
+                  isSearchable: !_prev.isSearchable,
+                };
+              });
+              setValue([]);
+            }}
+          >
+            Searchable: {configure.isSearchable ? "ON" : "OFF"}
+          </button>
+          <button
+            className="rounded-full"
+            onClick={() => {
+              setConfigure((_prev) => {
+                return {
+                  ..._prev,
+                  outlined: !_prev.outlined,
+                };
+              });
+              setValue([]);
+            }}
+          >
+            Outlined: {configure.outlined ? "ON" : "OFF"}
+          </button>
+        </div>
+
+        <pre className="bg-gray-300 p-5 mt-10 border border-gray-500 rounded-sm max-w-80">
+          props: <br />
+          <ul>
+            <li>searchable: {configure.isSearchable ? "True" : "False"}</li>
+            <li>multiple: {configure.isMultiple ? "True" : "False"}</li>
+            <li>outlined: {configure.outlined ? "True" : "False"}</li>
+          </ul>
+        </pre>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ui/Select.tsx
@@ -0,0 +1,60 @@
+import {
+  SelectRoot,
+  SelectTrigger,
+  SelectValue,
+  SelectDropdown,
+  type SelectOptionProps,
+} from "@hrdi/searchable-select";
+
+import { MagnifyingGlassIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+
+import { twMerge } from 'tailwind-merge';
+
+type SelectProps = {
+  multiple?: boolean;
+  value?: SelectOptionProps | SelectOptionProps[];
+  searchable?: boolean;
+  options?: SelectOptionProps[];
+  onChange: (value: SelectOptionProps | SelectOptionProps[]) => void;
+  outline?: boolean;
+};
+
+export default function Select({
+  options,
+  multiple = true,
+  searchable = true,
+  onChange,
+  value,
+  outline = false,
+}: SelectProps) {
+
+  return (
+    <>
+      <SelectRoot
+        options={options as SelectOptionProps[]}
+        value={value}
+        onChange={onChange}
+        multiple={multiple}
+        searchable={searchable}
+      >
+        <SelectTrigger>
+          <div className={twMerge('flex min-h-10 w-full flex-wrap items-center rounded-xs bg-gray-200 px-2 py-1 text-sm shadow-xs focus-within:ring-2 focus-within:ring-blue-500 justify-between gap-2', outline ? 'border border-gray-300 bg-transparent' : 'border border-transparent')}>
+            <SelectValue placeholder="" />
+            <ChevronDownIcon className="justify-self-end" />
+          </div>
+        </SelectTrigger>
+
+        <SelectDropdown
+          portal={false}
+          searchOptions={{
+            searchIcon: <MagnifyingGlassIcon width={"1em"} height={"1em"} />,
+            clear: true,
+            markOnSearch: true,
+          }}
+          options={options}
+          searchPosition="top"
+        ></SelectDropdown>
+      </SelectRoot>
+    </>
+  );
+}
